fc64d2a2f45632bd57f9dbe92755b72139e3330f
KaliWahyu3124 <138306241+KaliWahyu3124@users.noreply.github.com>
2024-03-06 22:10:24 +0700

    Update simple_llm_hf.py

--- simple_llm_hf.py
+++ simple_llm_hf.py
@@ -2,7 +2,7 @@
 import os
 from langchain_community.llms import HuggingFaceEndpoint
 
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "YOUR API KEY"
 llm = HuggingFaceEndpoint(repo_id="google/flan-ul2")
 text = "Beritahu fakta menarik tentang kentang!"
 result = llm.invoke(text)
