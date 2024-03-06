93500bbf7d9f68a46940ed36cadf9ccbb918723b
KaliWahyu3124 <138306241+KaliWahyu3124@users.noreply.github.com>
2024-03-06 22:08:00 +0700

    Update chain_customerSupport.py

--- chain_customerSupport.py
+++ chain_customerSupport.py
@@ -5,7 +5,7 @@ from langchain.prompts import PromptTemplate
 import os
 
 # Mengatur LLM
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "YOUR API KEY"
 llm = ChatOpenAI(temperature=0.9)
 
 def handle_complaint(komplain: str) -> str:
